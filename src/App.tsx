1e4bdc2773377b0560ec92789b3a7bca9121f47b
zestyitaliano <steelepalombo@gmail.com>
2025-11-23 19:16:47 +0000

    Update project filsdfsdfsdf

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+// src/App.tsx
+function App() {
+  return (
+    <main style={{ padding: '2rem', fontFamily: 'system-ui, sans-serif' }}>
+      <h1>No Gatekeeping Tools</h1>
+      <p>Pick a tool to open:</p>
+
+      <ul>
+        <li>
+          <a href="/src/pages/color-picker/color-picker.html">Color Picker</a>
+        </li>
+        <li>
+          <a href="/src/pages/case-converter/case-converter.html">Case Converter</a>
+        </li>
+        <li>
+          <a href="/src/pages/image-compressor/image-compressor.html">Image Compressor</a>
+        </li>
+        {/* add more links as needed */}
+      </ul>
+    </main>
+  );
+}
+
+export default App;
